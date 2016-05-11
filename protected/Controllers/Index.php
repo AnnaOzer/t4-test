ce0ee4e3390fe00f0b77398813cc034bb7e42ee5
AnnaOzer <ozerskaya.ru@mail.ru>
2016-05-11 16:05:15 +0400

    + $this->app->config->db    и    $this->app->db

--- protected/Controllers/Index.php
+++ protected/Controllers/Index.php
@@ -8,23 +8,33 @@ class Index
     extends Controller
 {
 
-    protected function beforeAction($action)
-    {
-        echo 'Before!';
-        return true;
 
-    }
 
     public function actionDefault()
     {
-        echo 'Test'; // ничего не выведет
+        var_dump( $this->app->config->db->default ); // объект класса Config
+        var_dump( $this->app->db ); // объект класса Connections
+        die;
     }
 
-    public function afterAction($action)
-    {
-        echo 'After!';
-    }
+
 }
 
+/*
+object(T4\Core\Config)#6 (2) { ["__path":protected]=> NULL ["__data":protected]=> array(5)
+    { ["driver"]=> string(5) "mysql"
+    ["host"]=> string(9) "127.0.0.1"
+    ["user"]=> string(4) "root"
+    ["password"]=> string(0) ""
+    ["dbname"]=> string(2) "t4" } }
 
-// Before!TestAfter!
+object(T4\Dbal\Connections)#40 (1)
+{ ["__data":protected]=> array(1) { ["default"]=> object(T4\Core\Config)#6 (2) { ["__path":protected]=> NULL ["__data":protected]=>
+    array(5) { ["driver"]=> string(5) "mysql"
+        ["host"]=> string(9) "127.0.0.1"
+        ["user"]=> string(4) "root"
+        ["password"]=> string(0) ""
+        ["dbname"]=> string(2) "t4"
+} } } }
+*/
+
--- protected/config.php
+++ protected/config.php
@@ -1,6 +1,17 @@
 <?php
 
 return [
+
+    'db' => [
+        'default' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'user' => 'root',
+            'password' => '',
+            'dbname' => 't4'
+        ]
+    ],
+
     'name' => 'Мой супер пупер личный проект',
     'extensions' => ['bootstrap' => ['theme' => 'cosmo'] ]
 ];
