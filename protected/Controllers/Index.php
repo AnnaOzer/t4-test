83e2e7066bf193a7df2baa7c7d9515836b5d4afe
AnnaOzer <ozerskaya.ru@mail.ru>
2016-05-11 16:14:28 +0400

    + выполнение запроса к базе через $this->app->db

--- protected/Controllers/Index.php
+++ protected/Controllers/Index.php
@@ -12,29 +12,11 @@ class Index
 
     public function actionDefault()
     {
-        var_dump( $this->app->config->db->default ); // объект класса Config
-        var_dump( $this->app->db ); // объект класса Connections
+        var_dump( $this->app->db->default->execute('SELECT * FROM news') );
         die;
     }
 
 
 }
 
-/*
-object(T4\Core\Config)#6 (2) { ["__path":protected]=> NULL ["__data":protected]=> array(5)
-    { ["driver"]=> string(5) "mysql"
-    ["host"]=> string(9) "127.0.0.1"
-    ["user"]=> string(4) "root"
-    ["password"]=> string(0) ""
-    ["dbname"]=> string(2) "t4" } }
-
-object(T4\Dbal\Connections)#40 (1)
-{ ["__data":protected]=> array(1) { ["default"]=> object(T4\Core\Config)#6 (2) { ["__path":protected]=> NULL ["__data":protected]=>
-    array(5) { ["driver"]=> string(5) "mysql"
-        ["host"]=> string(9) "127.0.0.1"
-        ["user"]=> string(4) "root"
-        ["password"]=> string(0) ""
-        ["dbname"]=> string(2) "t4"
-} } } }
-*/
-
+// SQLSTATE[HY000] [1049] Unknown database 't4'
