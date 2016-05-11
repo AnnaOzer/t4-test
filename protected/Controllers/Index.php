b8fe8f4cffb979a3006adb7cf6d320077093f582
AnnaOzer <ozerskaya.ru@mail.ru>
2016-05-11 13:13:25 +0400

    ! начало урока 2. HMVC, предопределенные методы контроллеров

--- protected/Controllers/Index.php
+++ protected/Controllers/Index.php
@@ -2,16 +2,20 @@
 
 namespace App\Controllers;
 
-use T4\Core\Config;
 use T4\Mvc\Controller;
 
 class Index
     extends Controller
 {
 
+    protected function access($action)
+    {
+        echo $action;
+        die;
+    }
+
     public function actionDefault()
     {
-        // $config = new Config(ROOT_PATH_PROTECTED . '/config.php');
         $config = $this->app->config;
         $this->data->name = $config->name;
 
@@ -19,7 +23,4 @@ class Index
 
 
 }
-
-/*
- * https://github.com/AnnaOzer/t4-test/blob/master/protected/Controllers/Index.php#L14 - можно и так,
- * а можно и $this->app->config, что проще*/
+// Default
