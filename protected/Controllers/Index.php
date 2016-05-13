5672041fe7527b24b8af5ca81aa7a7b72f77f6e7
AnnaOzer <ozerskaya.ru@mail.ru>
2016-05-13 17:08:43 +0400

    + страница pages/one, контроллер экшен шааблон

--- protected/Controllers/Index.php
+++ protected/Controllers/Index.php
@@ -2,31 +2,15 @@
 
 namespace App\Controllers;
 
-use T4\Core\Std;
-use T4\Mvc\Controller;
-use T4\Mvc\Route;
 
-class Params
-extends Std {}
+use T4\Mvc\Controller;
+
+
+
 class Index
     extends Controller
 {
-    public function actionDefault(Params $ids)
-    {
-        var_dump($ids);
-        die;
+    public function actionDefault() {
+
     }
-
-
 }
-// http://t4-test/?ids[]=42&ids[]=678&ids[]=586
-/* object(App\Controllers\Params)#48 (1)
-    { ["__data":protected]=> array(3) { [0]=> string(2) "42" [1]=> string(3) "678"
-        [2]=> string(3) "586" } }
-*/
-
-// http://t4-test/?ids[foo]=42&ids[bar]=678&ids[baz]=586
-/* object(App\Controllers\Params)#48 (1) { ["__data":protected]=> array(3)
-{ ["foo"]=> string(2) "42" ["bar"]=> string(3)
-"678" ["baz"]=> string(3) "586" } }
-*/
