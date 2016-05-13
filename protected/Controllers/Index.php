7fb17ccf2359bc86b4c3206b906656b3170c0140
AnnaOzer <ozerskaya.ru@mail.ru>
2016-05-13 14:26:05 +0400

    + объекты класса Route, роутинг

--- protected/Controllers/Index.php
+++ protected/Controllers/Index.php
@@ -3,15 +3,19 @@
 namespace App\Controllers;
 
 use T4\Mvc\Controller;
+use T4\Mvc\Route;
 
 class Index
     extends Controller
 {
     public function actionDefault()
     {
-        $this->app->flash->message = 'Hello!';
-        var_dump($this->app->flash->message); // Hello!
-        var_dump($this->app->flash->message); // NULL
+        $route = new Route('/Shop/Goods/One');
+        echo $route; // /Shop/Goods/One
+        echo $route->controller; // Goods
+        echo $route->action; // One
+        echo $route->params; //  ///
+        echo $route->module; // Shop
         die;
 
 }}
