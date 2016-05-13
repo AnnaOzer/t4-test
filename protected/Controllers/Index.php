28dd7e93fc42a104e90938c3b612affea159abb7
AnnaOzer <ozerskaya.ru@mail.ru>
2016-05-13 16:39:59 +0400

    + передача аргументов в контроллер - приведение к объекту класса

--- protected/Controllers/Index.php
+++ protected/Controllers/Index.php
@@ -2,20 +2,31 @@
 
 namespace App\Controllers;
 
+use T4\Core\Std;
 use T4\Mvc\Controller;
 use T4\Mvc\Route;
 
+class Params
+extends Std {}
 class Index
     extends Controller
 {
-    public function actionDefault(int $id = 0, $name = null)
+    public function actionDefault(Params $ids)
     {
-        echo 'Id: ' . $id;
-        echo 'Name: ' . $name;
+        var_dump($ids);
         die;
     }
 
 
 }
+// http://t4-test/?ids[]=42&ids[]=678&ids[]=586
+/* object(App\Controllers\Params)#48 (1)
+    { ["__data":protected]=> array(3) { [0]=> string(2) "42" [1]=> string(3) "678"
+        [2]=> string(3) "586" } }
+*/
 
-
+// http://t4-test/?ids[foo]=42&ids[bar]=678&ids[baz]=586
+/* object(App\Controllers\Params)#48 (1) { ["__data":protected]=> array(3)
+{ ["foo"]=> string(2) "42" ["bar"]=> string(3)
+"678" ["baz"]=> string(3) "586" } }
+*/
