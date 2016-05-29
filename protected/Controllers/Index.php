e42c3625c268e89f37221611a4e1faa5f2bf1e29
AnnaOzer <ozerskaya.ru@mail.ru>
2016-05-29 12:03:21 +0400

    ! Начало урока 3. Пример соединения с БД из Index контроллера

--- protected/Controllers/Index.php
+++ protected/Controllers/Index.php
@@ -2,17 +2,30 @@
 
 namespace App\Controllers;
 
-
-use App\Models\Article;
-use App\Models\News;
+use T4\Dbal\Connection;
 use T4\Mvc\Controller;
-
+use T4\Core\Config;
 
 
 class Index
     extends Controller
 {
-    public function actionDefault() {
+    public function actionDefault()
+    {
+        // конфиг
+        $config = new Config([
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'user' => 'root',
+            'password' => '',
+            'dbname' => 't4test'
+        ]);
+
+        // соединение
+        $conn = new Connection($config);
+
+        var_dump($conn);
+        die;
 
     }
 }
