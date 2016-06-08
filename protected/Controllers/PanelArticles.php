7dcb348935ebdc14f7ab2eb7462933b7170de60c
AnnaOzer <ozerskaya.ru@mail.ru>
2016-06-08 23:36:22 +0400

    + дз 3.3 админ панель новостей с редактированием

--- protected/Controllers/PanelArticles.php
+++ protected/Controllers/PanelArticles.php
@@ -14,4 +14,35 @@ class Panelarticles
         $this->data->items = Article::findAll();
     }
 
+    public function actionAdd()
+    {
+    }
+    
+    public function actionSave($article)
+    {
+        $item = (new Article())->fill($article)->save();
+        $this->redirect('/panelarticles/');
+    }
+    
+    public function actionEdit($id) 
+    {
+        $item = Article::findByPK($id);
+        $this->data->item = $item;
+    }
+
+    public function actionUpdate($id, $article)
+    {
+        $item = (Article::findByPK($id))->fill($article)->save();
+        $this->redirect('/panelarticles/');
+    }
+    
+    public function actionDelete($id)
+    {
+        $article = Article::findByPK($id);
+        if(!empty($article)) {
+            $article->delete(); 
+        }
+        $this->redirect('/panelarticles/');
+    }
+
 }
